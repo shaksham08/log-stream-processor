c534d546bcdd4b42883e53cc43544fd266f16b0a
shaksham08 <shakshamsinha@outlook.com>
2024-05-20 07:56:58 +0530

    dev:added comment

--- pkg/processor/processor.go
+++ pkg/processor/processor.go
@@ -6,7 +6,7 @@ import "github.com/shaksham08/log-stream-processor/pkg/models"
 func ProcessEvent(event models.Event) models.Event {
 	// Example: Adding a timestamp or modifying the event's body.
 	// This is a placeholder for your processing logic.
-	switch e := event.(type) {
+	switch e := event.(type) { // Type assertion
 	case models.SystemLog:
 		e.Body += " - Processed"
 		return e
