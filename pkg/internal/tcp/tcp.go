bb39146ed466b6df99ebe258eeb1a2824e9cc7bb
shaksham08 <shakshamsinha@outlook.com>
2024-05-23 00:07:19 +0530

    dev:added changes

--- pkg/internal/tcp/tcp.go
+++ pkg/internal/tcp/tcp.go
@@ -3,25 +3,46 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-func main() {
+func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		chunk := make([]byte, 1024)
+		readBytes, err := conn.Read(chunk)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			return
+		}
+		fmt.Printf("Received data: %v", string(chunk[:readBytes]))
+	}
+}
+
+func InitTCP(wg *sync.WaitGroup) {
+	defer wg.Done()
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		return
 	}
-	fmt.Println("Listening on 8080")
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting: ", err.Error())
-		return
+	var wg_tcp sync.WaitGroup
+	for {
+		fmt.Println("Listening on 8080")
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			return
+		}
+		fmt.Println("Accepted connection")
+		wg_tcp.Add(1)
+		go handleConnection(conn, &wg_tcp)
 	}
-	chunk := make([]byte, 1024)
-	readBytes, err := conn.Read(chunk)
-	if err != nil {
-		fmt.Println("Error reading: ", err.Error())
-		return
-	}
-	fmt.Printf("Received data: %v", string(chunk[:readBytes]))
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go InitTCP(&wg)
+	wg.Wait()
 }
